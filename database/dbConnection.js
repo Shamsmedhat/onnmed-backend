06c816aef1ad658e749eb4c76f018044b27aa0a7
Shamsmedhat <125091923+Shamsmedhat@users.noreply.github.com>
2025-04-17 04:11:31 +0200

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export const dbConnection = mongoose.connect('mongodb://localhost:27017/onnmed-db')
+export const dbConnection = mongoose.connect('mongodb+srv://shamsmedhat1:QjW6D8Drucnyva8k@cluster0.cfoczsm.mongodb.net/onnmed-db')
 .then(()=> console.log('database connected successfully' ))
 // mongodb+srv://shamsmedhat1:QjW6D8Drucnyva8k@cluster0.cfoczsm.mongodb.net/onnmed-db
 // mongodb://localhost:27017/onnmed-db
